a1de44b8817ff563efbbbe0add20700b7637c5f7
mateuszjanel <m.janel@student.pb.edu.pl>
2019-07-01 16:14:57 +0200

    Dodaj pliki projektów.

---
+++ CarTestManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarTestManager.Startup))]
+namespace CarTestManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
