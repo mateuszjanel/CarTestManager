387c1c93cd3d375f5292a3fb2bfc7866696375f9
mateuszjanel <janel.mateusz@gmail.com>
2019-07-02 14:44:32 +0200

    Connection string, migration and database created.

--- DatabaseLayer/Entities/Test.cs
+++ DatabaseLayer/Entities/Test.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DatabaseLayer.Entities
 {
@@ -10,6 +12,7 @@ namespace DatabaseLayer.Entities
     {
         [Key] public int Id { get; set; }
         [ForeignKey("Car")]
+        public int CarId { get; set; }
         public Car Car { get; set; }
         public float TimeToHundred { get; set; }
     }
